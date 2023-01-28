0e31d06f7e21881aa06c02c4829268a562574a87
Mauricio Faustino <mauricio.faustino@gmail.com>
2023-01-28 16:08:15 -0300

    removes client field not utilized

--- src/main/java/br/com/bxblue/pokecoin/consumer/MercadoBitcoinAPIConsumer.java
+++ src/main/java/br/com/bxblue/pokecoin/consumer/MercadoBitcoinAPIConsumer.java
@@ -4,11 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.http.HttpClient;
-
 
 public class MercadoBitcoinAPIConsumer {
-    private final HttpClient client = HttpClient.newHttpClient();
 
     public Double getBtcPrice() {
         RestTemplate restTemplate = new RestTemplate();
