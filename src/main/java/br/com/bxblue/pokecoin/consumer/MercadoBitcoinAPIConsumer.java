8276fca18c84c64d02c4b2e36be312edf690d472
Mauricio Faustino <mauricio.faustino@gmail.com>
2023-01-28 16:38:45 -0300

    adds request header to fix heroku api call

--- src/main/java/br/com/bxblue/pokecoin/consumer/MercadoBitcoinAPIConsumer.java
+++ src/main/java/br/com/bxblue/pokecoin/consumer/MercadoBitcoinAPIConsumer.java
@@ -4,12 +4,21 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
+
 
 public class MercadoBitcoinAPIConsumer {
-
+    public static void main(String[] args) {
+        MercadoBitcoinAPIConsumer consumer = new MercadoBitcoinAPIConsumer();
+        System.out.println(consumer.getBtcPrice());
+    }
     public Double getBtcPrice() {
         RestTemplate restTemplate = new RestTemplate();
         String url = "https://www.mercadobitcoin.net/api/BTC/ticker/";
+        restTemplate.setInterceptors(Collections.singletonList((request, body, execution) -> {
+            request.getHeaders().add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+            return execution.execute(request, body);
+        }));
         String jsonResponse = restTemplate.getForObject(url, String.class);
         Gson gson = new Gson();
         JsonObject json = gson.fromJson(jsonResponse, JsonObject.class);
--- src/main/java/br/com/bxblue/pokecoin/consumer/PokeAPIConsumer.java
+++ src/main/java/br/com/bxblue/pokecoin/consumer/PokeAPIConsumer.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.List;
 
 public class PokeAPIConsumer {
@@ -14,6 +15,10 @@ public class PokeAPIConsumer {
         try {
             RestTemplate restTemplate = new RestTemplate();
             String url = "https://pokeapi.co/api/v2/pokemon/" + id;
+            restTemplate.setInterceptors(Collections.singletonList((request, body, execution) -> {
+                request.getHeaders().add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+                return execution.execute(request, body);
+            }));
             String jsonResponse = restTemplate.getForObject(url, String.class);
             ObjectMapper mapper = new ObjectMapper();
             mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
@@ -28,6 +33,10 @@ public class PokeAPIConsumer {
         try {
             RestTemplate restTemplate = new RestTemplate();
             String url = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+            restTemplate.setInterceptors(Collections.singletonList((request, body, execution) -> {
+                request.getHeaders().add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+                return execution.execute(request, body);
+            }));
             String jsonResponse = restTemplate.getForObject(url, String.class);
             ObjectMapper mapper = new ObjectMapper();
             mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
